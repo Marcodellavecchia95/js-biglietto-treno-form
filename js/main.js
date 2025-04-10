b6f48f9c6cc5ec521231140a427dfe7b4b3d5cae
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-04-10 15:33:59 +0200

    add html

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// const travelDistance = parseInt(prompt("Quanti km dovrai effettuare?"));
+// const userAge = parseInt(prompt("Quanti anni hai?"));
+// let outputText;
+// console.log(travelDistance, userAge);
+
+// const finalPrice = travelDistance * 0.21;
+// let discountUnder18 = (finalPrice * 20) / 100;
+// let discountOver65 = (finalPrice * 40) / 100;
+// if (userAge < 18) {
+//   outputText =
+//     "Il biglietto per minorenni costa €" +
+//     (finalPrice - discountUnder18).toFixed(2);
+// } else if (userAge > 65) {
+//   outputText =
+//     "Il biglietto per Over 65 costa €" +
+//     (finalPrice - discountOver65).toFixed(2);
+// } else {
+//   outputText = "Il biglietto costa €" + finalPrice.toFixed(2);
+// }
+
+// alert(outputText);
